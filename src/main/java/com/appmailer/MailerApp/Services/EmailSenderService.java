fdb1ce2e05cd0610b65db69a61d446e451f85280
OderoOluoch <oderoluoch@gmail.com>
2021-10-11 14:44:10 +0300

    simple method to send a simple email

--- src/main/java/com/appmailer/MailerApp/Services/EmailSenderService.java
+++ src/main/java/com/appmailer/MailerApp/Services/EmailSenderService.java
@@ -1,4 +1,26 @@
 package com.appmailer.MailerApp.Services;
 
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
 public class EmailSenderService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    public void sendSimpleEmail(String toEmail,String body,String subject){
+        SimpleMailMessage mailMessage = new SimpleMailMessage();
+
+        mailMessage.setFrom("ogeoffrey17@gmail.com");
+        mailMessage.setTo(toEmail);
+        mailMessage.setText(body);
+        mailMessage.setSubject(subject);
+
+        mailSender.send(mailMessage);
+        System.out.println("Simple email sent.. ");
+    }
 }
